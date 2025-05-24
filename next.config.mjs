12f58b5c4ddaca73a0e3e8aef7fd0c6e42d6ea7b
Muogbo Emmanuel <muogboemmanuel9@gmail.com>
2025-05-24 14:42:30 +0100

    add error page and static files

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,7 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    unoptimized: true,
   },
   experimental: {
     webpackBuildWorker: true,
@@ -32,7 +33,8 @@ const nextConfig = {
   swcMinify: true,
   poweredByHeader: false,
   reactStrictMode: true,
-  output: "standalone",
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  trailingSlash: true,
 };
 
 mergeConfig(nextConfig, userConfig);
